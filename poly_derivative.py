65bf9938f0aae585004b545e64723cdfee5184a6
Charlie1289 <123171619+Charlie1289@users.noreply.github.com>
2023-01-27 14:01:57 +0000

    done

--- poly_derivative.py
+++ poly_derivative.py
@@ -8,12 +8,19 @@ a3 = 0.2
 x0 = 3.1
 
 
-p_prime = p_prime = a1+2*a2*x0+3*a3*x0^2
+p_prime =a1+2*a2*x0+3*a3*x0**2
+
+a=[a0,a1,a2,a3]
+n=(0,1,2,3)
+y=0
+for element in a:
+ p_prime+=element*x0**n[y-1]*y
 
 
-
-
-
+eval_derivative(a, x0,m)
+for element in a:
+ p_prime+=element*x0**n[y-1]*y
 
 # This will be True if the code is correct
+print(p_prime)
 print(p_prime == 32.286)
